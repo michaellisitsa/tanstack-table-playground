7681e36257904500fb07115f6108662737b41763
Michael Lisitsa <michaellisitsa@Michaels-MacBook-Pro.local>
2023-12-08 20:49:53 +1100

    Add tanstack table dependency and cleanup nextjs default markup and css

---
+++ src/app/simple-table/page.tsx
@@ -0,0 +1,9 @@
+import styles from "./styles.module.css";
+
+import React from "react";
+
+function SimpleTable() {
+  return <h1 className={styles.heading}>SimpleTable</h1>;
+}
+
+export default SimpleTable;
